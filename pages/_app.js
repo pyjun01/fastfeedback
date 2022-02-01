f1fc081d2b95a1aa4354d6060f876e06cfcd4986
pyjun01 <pyjun02@gmail.com>
2022-02-01 22:48:00 +0900

    Change firebase version & Apply chakra-ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,39 @@
-import { AuthProvider } from '../lib/auth';
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import { css, Global } from '@emotion/react';
+
+import theme from '@/styles/theme';
+import { AuthProvider } from '@/lib/auth';
+
+function Style() {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+    </>
+  );
+}
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ChakraProvider theme={theme}>
+      <Style />
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   );
 }
 
